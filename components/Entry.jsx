53fc4f8c26dc7b0d7afaa038d179b9181364510e
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-13 05:08:38 +0100

    Add file

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -15,7 +15,7 @@ export default function Entry(props) {
             <div className="info-container">
                 <img 
                     className="marker"
-                    src="../images/marker.png" 
+                    src="public/images/marker.png" 
                     alt="map marker icon"
                 />
                 <span className="country">{props.country}</span>

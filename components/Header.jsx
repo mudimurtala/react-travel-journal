905e23a57d2d08132501155db332605d03a9b3e4
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-18 11:52:23 +0100

    Add files

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <header>
             <img src="public/images/globe.png" alt="globe icon" />
-            <h1>my travel journal.</h1>
+            <h1>my travel journal</h1>
         </header>
     )
 }

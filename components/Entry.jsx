194d20c64cbdb3085ad51b4459e3feaa1e8ac13c
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-18 14:54:17 +0100

    Add files

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,11 +1,15 @@
 import marker from "/images/marker.png"
 
 export default function Entry(props) {
+    console.log(props)
     return (
         <article className="journal-entry">
             <div className="main-image-container">
-                <img className="main-image"
-                src="https://scrimba.com/links/travel-journal-japan-image-url" alt="mount fuji" />
+                <img 
+                    className="main-image"
+                    src={props.img.src}
+                    alt={props.img.src}
+                />
             </div>
             <div className="info-container">
                 <img
@@ -13,13 +17,11 @@ export default function Entry(props) {
                     src={marker}
                     alt="map marker icon"
                 />
-                <span className="country">Japan</span>
-                <a href="https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9">view on Google Maps</a>
-                <h2 className="entry-title">Mount Fuji</h2>
-                <p className="trip-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="entry-text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 
-                feet). Mount Fuji is the single most popular tourist site in Japan, for both
-                Japanese and foreign tourists.</p>
+                <span className="country">{props.country}</span>
+                <a href={props.googleMapsLink}>view on Google Maps</a>
+                <h2 className="entry-title">{props.title}</h2>
+                <p className="trip-dates">{props.dates}</p>
+                <p className="entry-text">{props.text}</p>
             </div>
         </article>
     )

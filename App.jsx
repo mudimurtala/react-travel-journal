dbf4bae3b3514bb9d0192b1fd601f89463c236b5
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-18 11:52:51 +0100

    Add files

--- App.jsx
+++ App.jsx
@@ -1,23 +1,12 @@
 import Header from "./components/Header"
 import Entry from "./components/Entry"
-import data from "./data"
 
 export default function App() {
-    
-    const entryElements = data.map((entry) => {
-        return (
-            <Entry
-                key={entry.id}
-                {...entry}
-            />
-        )
-    })
-    
     return (
         <>
             <Header />
             <main className="container">
-                {entryElements}
+                <Entry />
             </main>
         </>
     )

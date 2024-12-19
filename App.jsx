9fdf44542ce368a4ffc01df3d443c7c968cdcc16
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-19 11:19:31 +0100

    Add files

--- App.jsx
+++ App.jsx
@@ -18,6 +18,18 @@ export default function App() {
                     dates="12 Jan, 2021 - 24 Jan, 2021"
                     text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
                 />
+
+<Entry
+                    img={{
+                        src: "https://scrimba.com/links/travel-journal-japan-image-url",
+                        alt: "Mount Fuji"
+                    }}
+                    title="Mount Fuji"
+                    country="Japan"
+                    googleMapsLink="https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9"
+                    dates="12 Jan, 2021 - 24 Jan, 2021"
+                    text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+                />
             </main>
         </>
     )

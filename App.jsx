fb4b1323833c2666c6d12cea2a7642fd9ab55c68
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-19 11:46:41 +0100

    Add files

--- App.jsx
+++ App.jsx
@@ -1,35 +1,34 @@
 import Header from "./components/Header"
 import Entry from "./components/Entry"
+import data from "./data"
+
+/**
+ * Challenge:
+ * - import the array of data from data.js
+ * - map over the array to create an <Entry /> component
+ *   for every item in the data array.
+ * - display the array of Entry components in place of the current
+ *   hard-coded <Entry /> instance.
+ */
+
 
 
 export default function App() {
+    const dataElements = data.map((item) => {
+        return <Entry 
+                    img={}
+                    title={item.title}
+                    country={item.country}
+                    googleMapsLink={item.googleMapsLink}
+                    dates={item.dates}
+                    text={item.text}
+                />
+    })
     return (
         <>
             <Header />
             <main className="container">
-                <Entry
-                    img={{
-                        src: "https://scrimba.com/links/travel-journal-japan-image-url",
-                        alt: "Mount Fuji"
-                    }}
-                    title="Mount Fuji"
-                    country="Japan"
-                    googleMapsLink="https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9"
-                    dates="12 Jan, 2021 - 24 Jan, 2021"
-                    text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-                />
-
-<Entry
-                    img={{
-                        src: "https://scrimba.com/links/travel-journal-japan-image-url",
-                        alt: "Mount Fuji"
-                    }}
-                    title="Mount Fuji"
-                    country="Japan"
-                    googleMapsLink="https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9"
-                    dates="12 Jan, 2021 - 24 Jan, 2021"
-                    text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-                />
+                {dataElements}
             </main>
         </>
     )

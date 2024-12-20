b02cf39a817073f9837d3f18d425dbc0660dc0ae
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-20 03:29:16 +0100

    Add files

--- App.jsx
+++ App.jsx
@@ -18,7 +18,7 @@ export default function App() {
         return (
             <Entry
                     key={item.id}
-                    item={item}
+                    {...item}
                 />
         )
     })

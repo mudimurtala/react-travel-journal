47864e1512ab4b03b406979fc578829e6c444bb1
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-23 14:38:25 +0100

    Add an image link

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ export default [
     {
         id: 1,
         img: {
-            src: "public/images/imagebook.avif",
+            src: "https://scrimba.com/links/travel-journal-japan-image-url",
             alt: "Mount Fuji"
         },
         title: "Mount Fuji",
